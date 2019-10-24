9ffea101f39fef925256417aaf00694c71dde07b
Richard Hand <rhand@adobe.com>
2019-10-24 14:26:53 +0200

    [DataLayer] Prepare beta version of the Adobe Client Data Layer (#20)
    - moves listener manager -> ListenerManagerFactory
    - ListenerManager.registerListener -> ListenerManager.register
    - ListenerManager.unregisterListener -> ListenerManager.unregister
    - refactors JSDoc type definitions
    - document listener manager factory and listener manager
    - removes a few debug statements

--- src/scripts/DataLayerListenerManagerFactory.js
+++ src/scripts/DataLayerListenerManagerFactory.js
@@ -13,79 +13,99 @@ const constants = require('./DataLayerConstants');
 const has = require('lodash.has');
 const isEqual = require('lodash.isequal');
 
-const ListenerManager = function() {
-  const that = {};
+/**
+ * Factory that creates a listener manager.
+ *
+ * @typedef ListenerManagerFactory
+ */
+const ListenerManagerFactory = {};
+
+/**
+ * Creates a listener manager.
+ *
+ * @returns {ListenerManager} A listener manager.
+ */
+ListenerManagerFactory.create = function() {
   const _listeners = {};
 
   /**
-   * Registers a listener based on a listener on configuration.
+   * Listener Manager
    *
-   * @param {DataLayer.Item} listenerOn The listener on.
+   * @typedef ListenerManager
+   * @type {Object}
    */
-  that.registerListener = function(listenerOn) {
-    const eventName = listenerOn.config.on;
-    if (!_isRegisteredListener(listenerOn)) {
-      if (!_listeners[eventName]) {
-        _listeners[eventName] = [];
+  const ListenerManager = {
+    /**
+     * Registers a listener based on a listener on item.
+     *
+     * @function
+     * @param {Item} listenerOn The listener on.
+     */
+    register: function(listenerOn) {
+      const event = listenerOn.config.on;
+
+      if (Object.prototype.hasOwnProperty.call(_listeners, event)) {
+        if (!_isRegistered(listenerOn)) {
+          _listeners[event].push(listenerOn);
+        }
+      } else {
+        _listeners[event] = [listenerOn];
       }
-      _listeners[eventName].push(listenerOn);
+    },
+    /**
+     * Unregisters a listener based on a listener off item.
+     *
+     * @function
+     * @param {Item} listenerOff The listener off.
+     */
+    unregister: function(listenerOff) {
+      const indexes = _getListenersMatchingListenerOff(listenerOff);
+      const event = listenerOff.config.off;
 
-      console.debug('listener registered on: ', eventName);
-    }
-  };
-
-  /**
-   * Unregisters a listener.
-   *
-   * @param {DataLayer.Item} listenerOff The listener off.
-   */
-  that.unregisterListener = function(listenerOff) {
-    const indexes = _getListenersMatchingListenerOff(listenerOff);
-    const eventName = listenerOff.config.off;
-    for (let i = indexes.length - 1; i > -1; i--) {
-      if (indexes[i] > -1) {
-        _listeners[eventName].splice(indexes[i], 1);
-
-        console.debug('listener unregistered on: ', eventName);
+      for (let i = indexes.length - 1; i > -1; i--) {
+        if (indexes[i] > -1) {
+          _listeners[event].splice(indexes[i], 1);
+        }
       }
-    }
-  };
-
-  /**
-   * Triggers all the registered listeners matching the item.
-   *
-   * @param {DataLayer.Item} item The item.
-   */
-  that.triggerListeners = function(item) {
-    const triggeredEvents = _getTriggeredEvents(item);
-    triggeredEvents.forEach(function(eventName) {
-      if (_listeners[eventName]) {
-        _listeners[eventName].forEach(function(listener) {
-          that.callListenerHandler(listener, item);
-        });
+    },
+    /**
+     * Triggers all the registered listeners matching the item.
+     *
+     * @function
+     * @param {Item} item The item.
+     */
+    triggerListeners: function(item) {
+      const that = this;
+      const triggeredEvents = _getTriggeredEvents(item);
+      triggeredEvents.forEach(function(eventName) {
+        if (_listeners[eventName]) {
+          _listeners[eventName].forEach(function(listener) {
+            that.callListenerHandler(listener, item);
+          });
+        }
+      });
+    },
+    /**
+     * Calls the listener on the item if a match is found.
+     *
+     * @function
+     * @param {Item} listener The listener.
+     * @param {Item} item The item.
+     */
+    callListenerHandler: function(listener, item) {
+      if (_isMatching(listener, item)) {
+        const listenerConfig = listener.config;
+        const itemConfig = item.config;
+        const itemConfigCopy = JSON.parse(JSON.stringify(itemConfig));
+        listenerConfig.handler(itemConfigCopy);
       }
-    });
-  };
-
-  /**
-   * Calls the listener on the item if a match is found.
-   *
-   * @param {DataLayer.Item} listener The listener.
-   * @param {DataLayer.Item} item The item.
-   */
-  that.callListenerHandler = function(listener, item) {
-    if (_isMatching(listener, item)) {
-      const listenerConfig = listener.config;
-      const itemConfig = item.config;
-      const itemConfigCopy = JSON.parse(JSON.stringify(itemConfig));
-      listenerConfig.handler(itemConfigCopy);
     }
   };
 
   /**
    * Returns the names of the events that are triggered for this item.
    *
-   * @param {DataLayer.Item} item The item.
+   * @param {Item} item The item.
    * @returns {Array} An array with the names of the events that are triggered for this item.
    * @private
    */
@@ -93,14 +113,14 @@ const ListenerManager = function() {
     const triggeredEvents = [];
     const itemConfig = item.config;
     if (item.type === constants.itemType.DATA) {
-      triggeredEvents.push(constants.event.CHANGE);
+      triggeredEvents.push(constants.dataLayerEvent.CHANGE);
     } else if (item.type === constants.itemType.EVENT) {
-      if (itemConfig.event !== constants.event.CHANGE) {
+      if (itemConfig.event !== constants.dataLayerEvent.CHANGE) {
         triggeredEvents.push(itemConfig.event);
       }
-      triggeredEvents.push(constants.event.EVENT);
+      triggeredEvents.push(constants.dataLayerEvent.EVENT);
       if (itemConfig.data) {
-        triggeredEvents.push(constants.event.CHANGE);
+        triggeredEvents.push(constants.dataLayerEvent.CHANGE);
       }
     }
     return triggeredEvents;
@@ -109,8 +129,8 @@ const ListenerManager = function() {
   /**
    * Checks if the listener matches the item.
    *
-   * @param {DataLayer.Item} listener The listener.
-   * @param {DataLayer.Item} item The item.
+   * @param {Item} listener The listener.
+   * @param {Item} item The item.
    * @returns {Boolean} true if listener matches the item, false otherwise.
    * @private
    */
@@ -120,16 +140,16 @@ const ListenerManager = function() {
     let isMatching = false;
 
     if (item.type === constants.itemType.DATA) {
-      if (listenerConfig.on === constants.event.CHANGE) {
+      if (listenerConfig.on === constants.dataLayerEvent.CHANGE) {
         isMatching = _isSelectorMatching(listenerConfig, item);
       }
     } else if (item.type === constants.itemType.EVENT) {
-      if (listenerConfig.on === constants.event.EVENT ||
+      if (listenerConfig.on === constants.dataLayerEvent.EVENT ||
         listenerConfig.on === itemConfig.event) {
         isMatching = _isSelectorMatching(listenerConfig, item);
       }
       if (itemConfig.data &&
-        listenerConfig.on === constants.event.CHANGE) {
+        listenerConfig.on === constants.dataLayerEvent.CHANGE) {
         isMatching = _isSelectorMatching(listenerConfig, item);
       }
     }
@@ -139,7 +159,7 @@ const ListenerManager = function() {
   /**
    * Returns the indexes of the registered listeners that match the listener off.
    *
-   * @param {DataLayer.Item} listenerOff The listener off.
+   * @param {Item} listenerOff The listener off.
    * @returns {Array} The indexes of the matching listeners.
    * @private
    */
@@ -160,8 +180,8 @@ const ListenerManager = function() {
   /**
    * Checks whether the listener on matches the listener off.
    *
-   * @param {DataLayer.Item} listenerOff The listener off.
-   * @param {DataLayer.Item} listenerOn The listener on.
+   * @param {Item} listenerOff The listener off.
+   * @param {Item} listenerOn The listener on.
    * @returns {Boolean} true if the listener on matches the listener off, false otherwise.
    * @private
    */
@@ -187,11 +207,11 @@ const ListenerManager = function() {
   /**
    * Checks whether the listener is registered.
    *
-   * @param {DataLayer.Item} listenerOn The listener on.
+   * @param {Item} listenerOn The listener on.
    * @returns {Boolean} true if the listener is registered, false otherwise.
    * @private
    */
-  function _isRegisteredListener(listenerOn) {
+  function _isRegistered(listenerOn) {
     const eventName = listenerOn.config.on;
     if (_listeners[eventName]) {
       for (let i = 0; i < _listeners[eventName].length; i++) {
@@ -208,7 +228,7 @@ const ListenerManager = function() {
    * Checks if the given listenerConfig has a selector that points to an object in the data payload of the itemConfig.
    *
    * @param {ListenerOnConfig} listenerConfig Config of the listener to extract the selector from.
-   * @param {DataLayer.Item} item The item.
+   * @param {Item} item The item.
    * @returns {Boolean} true if a selector is not provided or if the given selector is matching, false otherwise.
    * @private
    */
@@ -221,7 +241,7 @@ const ListenerManager = function() {
     }
   }
 
-  return that;
+  return ListenerManager;
 };
 
-module.exports = ListenerManager;
+module.exports = ListenerManagerFactory;
