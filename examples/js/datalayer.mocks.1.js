abf3e165202ab6b332e51d581b3fa01d347f7806
Burkhard Pauli <pauli@adobe.com>
2020-04-23 17:23:10 +0200

    remove data namespace when pushing data to state object
    - remove obsolete tests

--- examples/js/datalayer.mocks.1.js
+++ examples/js/datalayer.mocks.1.js
@@ -42,7 +42,6 @@ governing permissions and limitations under the License.
         console.info('SUCCESS: test case 1 "add data"');
       }
 
-
       // -----------------------------------------------------------------------------------------------------------------
       // Test case 2: remove data
       // -----------------------------------------------------------------------------------------------------------------
@@ -61,7 +60,6 @@ governing permissions and limitations under the License.
         console.info('SUCCESS: test case 2 "remove data"');
       }
 
-
       // -----------------------------------------------------------------------------------------------------------------
       // Test case 3: add event with data
       // -----------------------------------------------------------------------------------------------------------------
@@ -78,13 +76,12 @@ governing permissions and limitations under the License.
         }
       });
 
-      const  id3 = adobeDataLayer.getState().component.carousel.carousel3.id;
+      const id3 = adobeDataLayer.getState().component.carousel.carousel3.id;
       if (id3 !== '/content/mysite/en/home/jcr:content/root/carousel3') {
         console.error('FAILS: test case 3 "add event with data"');
       } else {
         console.info('SUCCESS: test case 3 "add event with data"');
       }
-
 
       // -----------------------------------------------------------------------------------------------------------------
       // test case 4: listener on: datalayer:change
@@ -116,7 +113,6 @@ governing permissions and limitations under the License.
         console.info('SUCCESS: test case 4 "listener on: datalayer:change"');
       }
 
-
       // -----------------------------------------------------------------------------------------------------------------
       // test case 5: listener on: datalayer:event
       // -----------------------------------------------------------------------------------------------------------------
@@ -143,7 +139,6 @@ governing permissions and limitations under the License.
         console.info('SUCCESS: test case 5 "listener on: datalayer:event"');
       }
 
-
       // -----------------------------------------------------------------------------------------------------------------
       // test case 6: listener on: custom event
       // -----------------------------------------------------------------------------------------------------------------
@@ -169,7 +164,6 @@ governing permissions and limitations under the License.
       } else {
         console.info('SUCCESS: test case 6 "listener on: custom event"');
       }
-
 
       // -----------------------------------------------------------------------------------------------------------------
       // test case 7: listener on: scope = past
@@ -198,7 +192,7 @@ governing permissions and limitations under the License.
         console.info('SUCCESS: test case 7a "listener on: scope = past"');
       }
 
-      let  success7b = true;
+      let success7b = true;
 
       adobeDataLayer.push({
         on: 'carousel 7b clicked',
@@ -221,12 +215,11 @@ governing permissions and limitations under the License.
         console.info('SUCCESS: test case 7b "listener on: scope = past"');
       }
 
-
       // -----------------------------------------------------------------------------------------------------------------
       // test case 8: listener on: scope = future
       // -----------------------------------------------------------------------------------------------------------------
 
-      let  success8a = true;
+      let success8a = true;
 
       adobeDataLayer.push({
         event: 'carousel 8a clicked',
@@ -249,7 +242,7 @@ governing permissions and limitations under the License.
         console.info('SUCCESS: test case 8a "listener on: scope = future"');
       }
 
-      let  success8b = false;
+      let success8b = false;
 
       adobeDataLayer.push({
         on: 'carousel 8b clicked',
@@ -272,12 +265,11 @@ governing permissions and limitations under the License.
         console.info('SUCCESS: test case 8b "listener on: scope = future"');
       }
 
-
       // -----------------------------------------------------------------------------------------------------------------
       // test case 9: listener on: scope = all
       // -----------------------------------------------------------------------------------------------------------------
 
-      let  success9a = false;
+      let success9a = false;
 
       adobeDataLayer.push({
         event: 'carousel 9a clicked',
@@ -300,7 +292,7 @@ governing permissions and limitations under the License.
         console.info('SUCCESS: test case 9a "listener on: scope = all"');
       }
 
-      let  success9b = false;
+      let success9b = false;
 
       adobeDataLayer.push({
         on: 'carousel 9b clicked',
@@ -323,12 +315,11 @@ governing permissions and limitations under the License.
         console.info('SUCCESS: test case 9b "listener on: scope = all"');
       }
 
-
       // -----------------------------------------------------------------------------------------------------------------
       // test case 10: listener on: scope = undefined (default to 'future')
       // -----------------------------------------------------------------------------------------------------------------
 
-      let  success10a = true;
+      let success10a = true;
 
       adobeDataLayer.push({
         event: 'carousel 10a clicked',
@@ -350,7 +341,7 @@ governing permissions and limitations under the License.
         console.info('SUCCESS: test case 10a "listener on: scope = undefined"');
       }
 
-      let  success10b = false;
+      let success10b = false;
 
       adobeDataLayer.push({
         on: 'carousel 10b clicked',
@@ -372,12 +363,11 @@ governing permissions and limitations under the License.
         console.info('SUCCESS: test case 10b "listener on: scope = undefined"');
       }
 
-
       // -----------------------------------------------------------------------------------------------------------------
       // test case 11: listener off
       // -----------------------------------------------------------------------------------------------------------------
 
-      let  success11 = true;
+      let success11 = true;
 
       adobeDataLayer.push({
         on: 'carousel 11a clicked',
@@ -412,114 +402,6 @@ governing permissions and limitations under the License.
       } else {
         console.log('SUCCESS: test case 11 "listener off"');
       }
-
-
-      // -----------------------------------------------------------------------------------------------------------------
-      // test case 12: type validation: data
-      // -----------------------------------------------------------------------------------------------------------------
-
-      adobeDataLayer.push({
-        valid: {
-          data: true
-        }
-      });
-
-      if (!adobeDataLayer.getState().valid.data) {
-        console.error('FAILS: test case 12 "type validation: data"');
-      } else {
-        console.info('SUCCESS: test case 12 "type validation: data"');
-      }
-
-
-      // -----------------------------------------------------------------------------------------------------------------
-      // test case 13: type validation: event
-      // -----------------------------------------------------------------------------------------------------------------
-
-      adobeDataLayer.push({
-        event: 'clicked',
-        data: {
-          valid: true
-        }
-      });
-
-      if (!adobeDataLayer.getState().data.valid) {
-        console.error('FAILS: test case 13 "type validation: event"');
-      } else {
-        console.info('SUCCESS: test case 13 "type validation: event"');
-      }
-
-
-      // -----------------------------------------------------------------------------------------------------------------
-      // test case 14: type validation: listener on
-      // -----------------------------------------------------------------------------------------------------------------
-
-      let  success14 = false;
-
-      adobeDataLayer.push({
-        on: 'carousel 14 clicked',
-        handler: function(event) {
-          success14 = true;
-        }
-      });
-
-      adobeDataLayer.push({
-        event: 'carousel 14 clicked',
-        eventInfo: {
-          reference: '/content/mysite/en/home/jcr:content/root/carousel14'
-        }
-      });
-
-      if (!success14) {
-        console.error('FAILS: test case 14 "validation: listener on"');
-      } else {
-        console.info('SUCCESS: test case 14 "validation: listener on"');
-      }
-
-
-      // -----------------------------------------------------------------------------------------------------------------
-      // test case 15: type validation: listener off
-      // -----------------------------------------------------------------------------------------------------------------
-
-      let  success15 = false;
-
-      adobeDataLayer.push({
-        on: 'carousel 15 clicked',
-        handler: function(event) {
-          success15 = true;
-        }
-      });
-
-      adobeDataLayer.push({
-        event: 'carousel 15 clicked',
-        eventInfo: {
-          reference: '/content/mysite/en/home/jcr:content/root/carousel15'
-        }
-      });
-
-      // success15 should be: true: we force it to false:
-      success15 = false;
-
-      adobeDataLayer.push({
-        off: 'carousel 15 clicked',
-        handler: function(event) {
-          success15 = true;
-        },
-        invalid: 'invalid'
-      });
-
-      adobeDataLayer.push({
-        event: 'carousel 15 clicked',
-        eventInfo: {
-          reference: '/content/mysite/en/home/jcr:content/root/carousel15'
-        }
-      });
-
-      if (!success15) {
-        console.error('FAILS: test case 15 "type validation: listener off"');
-      } else {
-        console.info('SUCCESS: test case 15 "type validation: listener off"');
-      }
-
     }
   });
 
