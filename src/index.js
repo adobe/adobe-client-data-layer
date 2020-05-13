0971df60e0f8699d47dffb977e48c23be82ca1d2
Bartosz Glowacki <bartosz.glow@gmail.com>
2020-05-13 09:29:43 +0200

    dataLayerManager properly imported

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-const DataLayerManager = require('./DataLayerManager');
+const DataLayerManager = require('./dataLayerManager');
 
 /**
  * Data Layer.
